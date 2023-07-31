2baf21c1c163de9aa5ab92a78ccda46014950aa5
Aravind Sundararajan <Aravind.Sundararajan@protonmail.com>
2023-07-30 20:39:57 -0700

    fixing members parsing

--- moxygen/compound.py
+++ moxygen/compound.py
@@ -1,6 +1,5 @@
-from moxygen.logger import getLogger
-
 from itertools import chain
+
 
 class Compound:
     def __init__(self, parent=None, id='', name=''):
@@ -20,41 +19,68 @@ class Compound:
             self.compounds[id] = compound
         return compound
 
-    def to_array(self, type='compounds', kind=None):
-        arr = list(self.compounds.values()) if type == 'compounds' else self.members
+    def to_array(self, type_='compounds', kind=None):
+        if type_ == 'compounds':
+            arr = list(self.compounds.values())
+        else:
+            arr = self.members
 
         if kind:
-            arr = [compound for compound in arr if not kind or compound.kind == kind]
+            arr = [compound for compound in arr if compound.kind == kind]
+
         all_arr = []
         for compound in arr:
-            this_arr = compound.to_array(type, kind)
-            all_arr = all_arr + this_arr
-        return list(chain(*all_arr))
+            this_arr = compound.to_array(type_, kind)
+            all_arr.append(this_arr)
 
-    def to_filtered_array(self, type='compounds'):
-        all_arr = [item.to_filtered_array(type) for item in self.filtered.get(type, [])]
-        return list(chain(*all_arr))
+        if not all_arr:  # Base case: No members, return itself as a single-element list.
+            return [self]
+
+        return list(chain.from_iterable(all_arr))
+
+    def to_filtered_array(self, type_='compounds'):
+        all_arr = []
+        if type_ in self.filtered:
+            all_arr = [item.to_filtered_array(type_) for item in self.filtered[type_]]
+            if all_arr == []:
+                all_arr = all_arr.append(self)
+        print("ALL ARR")
+        print(all_arr)
+        flattened_arr = list(chain.from_iterable(all_arr))
+        return flattened_arr
 
     def filter_children(self, filters, groupid=None):
-        member_filter = ""
-        compound_filter = ""
+        member_filter = {}
+        compound_filter = {}
 
         if filters is not None:
             member_filter = filters['members']
             compound_filter = filters['compounds']
-
-        for compound in self.to_array('compounds'):
+        compounds = self.to_array()
+        for compound in compounds:
             compound.filtered['members'] = compound.filter(compound.members, 'section', member_filter, groupid)
             compound.filtered['compounds'] = compound.filter(compound.compounds, 'kind', compound_filter, groupid)
 
         self.filtered['members'] = self.filter(self.members, 'section',  member_filter, groupid)
         self.filtered['compounds'] = self.filter(self.compounds, 'kind', compound_filter, groupid)
 
-    def filter(self, collection, key, filters, groupid=None):
+    def filter(self, collections, key, filters, groupid=None):
         categories = {}
         result = []
-        if collection:
-            for name, item in collection.items():
+        if type(collections) == list:
+            for item in collections:
+                if item['kind'] == 'namespace' and 'compounds' not in item['filtered'] and 'members' not in item['filtered']:
+                    continue
+
+                if groupid and item[groupid] != groupid:
+                    continue
+                if key in list(item.keys()):
+                    if item[key] not in categories:
+                        categories[item[key]] = []
+
+                    categories[item[key]].append(item)
+        elif type(collections) == dict:
+            for name, item in collections.items():
                 if item:
                     if item.kind == 'namespace' and 'compounds' not in item.filtered and 'members' not in item.filtered:
                         continue
@@ -67,7 +93,6 @@ class Compound:
 
                     categories[item.__getattribute__(key)].append(item)
 
-            for category in filters:
-                result += categories.get(category, [])
-
+        for category in filters:
+            result += categories.get(category, [])
         return result
